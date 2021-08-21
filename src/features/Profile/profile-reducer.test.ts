2aa1d2da48cd19f9f5c34616e79d741c4caab2ff
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-08-21 20:08:12 +0300

    profile-reducer: add initial state, action creator to set user data, types. profile-reducer.test: add new correct test.

--- src/features/Profile/profile-reducer.test.ts
+++ src/features/Profile/profile-reducer.test.ts
@@ -1,10 +1,32 @@
-import {ProfileReducerInitialStateTypes} from "./profile-reducer";
+import {profileReducer, ProfileReducerInitialStateTypes, setUserData} from "./profile-reducer";
 
 
 let initialSate: ProfileReducerInitialStateTypes
 
 beforeEach(() => {
     initialSate = {
-
+        id: "",
+        email: "",
+        name: "",
+        avatar: "",
+        publicCardPacksCount: 0,
     }
+})
+
+test("user data should be set", () => {
+    const userData = {
+        id: "123",
+        email: "blabla@mail.com",
+        name: "Yo",
+        avatar: "",
+        publicCardPacksCount: 10,
+    }
+
+    const action = setUserData(userData)
+    const endState = profileReducer(initialSate, action)
+
+    expect(endState.id).toBe("123")
+    expect(endState.email).toBe("blabla@mail.com")
+    expect(endState.name).toBe("Yo")
+    expect(endState.publicCardPacksCount).toBe(10)
 })
--- src/features/Profile/profile-reducer.ts
+++ src/features/Profile/profile-reducer.ts
@@ -1,16 +1,41 @@
-export const profileReducerInitialState: ProfileReducerInitialStateTypes = {}
+export const profileReducerInitialState: ProfileReducerInitialStateTypes = {
+    id: "",
+    email: "",
+    name: "",
+    avatar: "",
+    publicCardPacksCount: 0,
+}
 
-export const loginReducer = (state = profileReducerInitialState, action: ProfileReducerActionTypes) => {
-
+export const profileReducer = (state = profileReducerInitialState, action: ProfileReducerActionTypes) => {
+    switch (action.type) {
+        case "PROFILE/SET_USER_DATA":
+            return {
+                ...state,
+                id: action.userData.id,
+                email: action.userData.email,
+                name: action.userData.name,
+                publicCardPacksCount: action.userData.publicCardPacksCount
+            }
+        default:
+            return state
+    }
 }
 
 
 /* Action creators */
-
+export const setUserData = (userData: ProfileReducerInitialStateTypes) =>
+    ({type: "PROFILE/SET_USER_DATA", userData} as const)
 
 /* Thunk creators */
 
 
 /* Types */
-export type ProfileReducerInitialStateTypes = any           //need to fix
-export type ProfileReducerActionTypes = any                 //need to fix
+export type ProfileReducerInitialStateTypes = {
+    id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+}
+export type ProfileReducerActionTypes =
+    |ReturnType<typeof setUserData>
