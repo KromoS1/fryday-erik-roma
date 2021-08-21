90d5b3d26f55ac0438822f6f5f99ddef6ec25dad
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-08-21 20:16:27 +0300

    Add ProfileContainer component

---
+++ src/features/Profile/ProfileContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Profile} from "./Profile";
+
+
+export const ProfileContainer = React.memo(() => {
+
+    return (
+        <>
+            <Profile/>
+        </>
+    )
+})
