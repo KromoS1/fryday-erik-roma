bf56f9a3e58a7c40670a7855f7d3fa125a3c25b5
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-08-23 15:06:42 +0300

    Add login api request

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,41 @@
 import axios from 'axios';
 
-
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: "http://localhost:7542/2.0",
     withCredentials: true,
 })
 
 export const apiPing = {
-    pingGet(){
+    pingGet() {
         return axiosInstance.get("/ping").then(response => {
             console.log(response)
         })
     }
+}
+export const authApi = {
+    login(params: LoginRequestParamsType) {
+        return axiosInstance.post<LoginResponseType>("/auth/login", {...params})
+    },
+    forgot() {
+
+    }
+}
+export type LoginResponseType = {
+    id: string
+    created: string
+    email: string
+    isAdmin: boolean
+    name: string
+    publicCardPacksCount: number
+    rememberMe: boolean
+    token: string
+    tokenDeathTime: number
+    updated: string
+    verified: boolean
+
+}
+export type LoginRequestParamsType = {
+    email: string
+    password: string
+    rememberMe: boolean
 }
--- src/features/Login/login-reducer.ts
+++ src/features/Login/login-reducer.ts
@@ -1,3 +1,7 @@
+import {AppThunkType} from "../../app/store";
+import {authApi, LoginRequestParamsType} from "../../api/api";
+import {setUserData} from "../Profile/profile-reducer";
+
 export const loginReducerInitialState: LoginReducerInitialStateTypes = {}
 
 export const loginReducer = (state = loginReducerInitialState, action: LoginReducerActionTypes) => {
@@ -9,7 +13,23 @@ export const loginReducer = (state = loginReducerInitialState, action: LoginRedu
 
 
 /* Thunk creators */
-
+export const loginTC = (loginParams: LoginRequestParamsType):AppThunkType => dispatch => {
+    authApi.login(loginParams)
+        .then(res => {
+            const userDataParams = {
+                id: res.data.id,
+                email: res.data.email,
+                name: res.data.name,
+                publicCardPacksCount: res.data.publicCardPacksCount,
+            }
+            dispatch(setUserData(userDataParams))
+            console.log(res)
+        })
+        .catch(e => {
+            const error = e.response ? e.response.data.error : (e.message + ', more details in the console')
+            console.log(error)
+        })
+}
 
 /* Types */
 export type LoginReducerInitialStateTypes = any           //need to fix
