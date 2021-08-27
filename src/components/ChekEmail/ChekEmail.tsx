9d3cb46fa6a51887965dd5f83fe152603cbe390b
Ash Barsov <a89875239469@gmail.com>
2021-08-27 17:52:27 +0300

    --add layout checkMail page --add redirect with local Email

--- src/components/ChekEmail/ChekEmail.tsx
+++ src/components/ChekEmail/ChekEmail.tsx
@@ -1,7 +1,21 @@
-import React, {FC} from 'react'
+import React, {FC} from 'react';
+import {useLocation} from "react-router-dom";
+import style from './ChekEmail.module.scss'
+import checkEmailImg from './../../resources/images/checkEmail.jpg';
 
-export const ChekEmail: FC = () => {
-    return(
-        <h1>Chek your email</h1>
-    )
+
+type ChekEmailPropsType = {}
+
+export const ChekEmail: FC<ChekEmailPropsType> = (props) => {
+    const location = useLocation<{userEmail: string}>();
+    return <div className={style.chekMail}>
+        <div className={style.title}>Cards</div>
+        <img src={checkEmailImg} className={style.authIcon} alt={'Check your Email page'}/>
+        <div className={style.titleName}>
+            <h3>Check Email</h3>
+        </div>
+        <div className={style.subtitle}>
+            We've sent an Email with instructions to {location.state ? location.state.userEmail : ' your Email'}
+        </div>
+    </div>
 }
