c96d9454b4c8888ded5d8e6ab1f1aab2230a8249
krakenHRI@mail.ru <krakenHRI@mail.ru>
2021-09-01 13:36:59 +0300

    update style app

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -29,34 +29,36 @@ const LoginForm: FC<PropsType> = props => {
 
     return (
         <div className={style.login}>
-            <div className={style.title}>Cards</div>
-            <div className={style.titleName}>
-                <h3>Sign In</h3>
-            </div>
-            <Form name={'login'}
-                  className={style.form}
-                  {...formItemLayout}
-                  form={form}
-                  onFinish={onFinish}>
-                {fieldComponent('email', 'E-mail')}
-                {fieldComponent('password', 'Password')}
-                <div className={style.forgot}>
+          <div className={style.container}>
+              <div className={style.title}>Cards</div>
+              <div className={style.titleName}>
+                  <h3>Sign In</h3>
+              </div>
+              <Form name={'login'}
+                    className={style.form}
+                    {...formItemLayout}
+                    form={form}
+                    onFinish={onFinish}>
+                  {fieldComponent('email', 'E-mail')}
+                  {fieldComponent('password', 'Password')}
+                  <div className={style.forgot}>
                     <span>
                         <NavLink to={'/recovery-password'}>
                             Forgot Password
                         </NavLink>
                     </span>
-                </div>
-                <div className={style.buttons}>
-                    <button className={style.btnLogin} type="submit">
-                        Login
-                    </button>
-                </div>
-                <div className={style.signUp}>
-                    <span>Don't have an account?</span>
-                    <NavLink to={"/registration"}>Sign Up</NavLink>
-                </div>
-            </Form>
+                  </div>
+                  <div className={style.buttons}>
+                      <button className={style.btnLogin} type="submit">
+                          Login
+                      </button>
+                  </div>
+                  <div className={style.signUp}>
+                      <span>Don't have an account?</span>
+                      <NavLink to={"/registration"}>Sign Up</NavLink>
+                  </div>
+              </Form>
+          </div>
         </div>
     )
 
